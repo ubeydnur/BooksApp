ba57fd47b9117579e0e0dc5b623ed8fa083b390b
Ubeydnur Karaburun <ubeydnurkaraburun@gmail.com>
2021-05-01 00:54:36 +0300

    2. yöntem: Provider --> Consumer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import BookList from './BookList'
+import BookContextProvider from '../contexts/BookContext'
 
 class App extends React.Component {
   render() {
     return (
       <div >
-        <BookList/>
+        <BookContextProvider>
+          <BookList />
+        </BookContextProvider>
       </div>
     )
   }
