587e3db2b7a6e778fbf7c1a4f0e3a031a1d97cb1
Ubeydnur Karaburun <ubeydnurkaraburun@gmail.com>
2021-05-01 21:11:01 +0300

    Multiple Context

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 export default class Book extends Component {
     render() {
         return (
-            <div className="col-lg-4 col-sm-6 mb-4">
-                <div className="portfolio-item">
-                    <a className="portfolio-link" data-toggle="modal" href="#">
-                        <div className="portfolio-hover">
-                            <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
+            <ThemeContext.Consumer>
+                {(contextTheme) => {
+                    const { isDartTheme, dark, light } = contextTheme
+                    const theme = isDartTheme ? dark : light
+                    return (
+                        <div className="col-lg-4 col-sm-6 mb-4">
+                            <div className="portfolio-item">
+                                <a className="portfolio-link" data-toggle="modal" href="#">
+                                    <div className="portfolio-hover" style={{background:theme.hover}}>
+                                        <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
+                                    </div>
+                                    <img className="img-fluid" src={this.props.book.imageURL} alt="..." />
+                                </a>
+                                <div className="portfolio-caption">
+                                    <div className="portfolio-caption-heading">{this.props.book.title}</div>
+                                    <div className="portfolio-caption-subheading text-muted">{this.props.book.author}</div>
+                                </div>
+                            </div>
                         </div>
-                        <img className="img-fluid" src={this.props.book.imageURL} alt="..." />
-                    </a>
-                    <div className="portfolio-caption">
-                        <div className="portfolio-caption-heading">{this.props.book.title}</div>
-                        <div className="portfolio-caption-subheading text-muted">{this.props.book.author}</div>
-                    </div>
-                </div>
-            </div>
+                    )
+                }}
+            </ThemeContext.Consumer>
         )
     }
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,31 +2,41 @@ import React, { Component } from 'react'
 import './BookList.css'
 import Book from './Book'
 import { BookContext } from '../contexts/BookContext'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 export default class BookList extends Component {
 
     render() {
 
         return (
-            <BookContext.Consumer>
-                {value => {
-                    return (
-                        <section className="page-section bg-light" id="portfolio">
-                            <div className="container">
-                                <div className="text-center">
-                                    <h2 className="section-heading">BOOKFOLIO</h2>
-                                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                                </div>
-                                <div className="row">
-                                    {value.books.map((book, i) => {
-                                        return <Book book={book} key={i} />
-                                    })}
-                                </div>
-                            </div>
-                        </section>
-                    )
-                }}
-            </BookContext.Consumer>
+            <ThemeContext.Consumer>
+                {(contextTheme) => (
+                    <BookContext.Consumer>
+                        {contextBook => {
+                            const { books } = contextBook
+                            const { isDartTheme, dark, light } = contextTheme
+                            const theme = isDartTheme ? dark : light
+                            return (
+                                <section
+                                    className="page-section"
+                                    style={{ background: theme.bg, color: theme.txt }} id="portfolio">
+                                    <div className="container">
+                                        <div className="text-center">
+                                            <h2 className="section-heading">BOOKFOLIO</h2>
+                                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                                        </div>
+                                        <div className="row">
+                                            {books.map((book, i) => {
+                                                return <Book book={book} key={i} />
+                                            })}
+                                        </div>
+                                    </div>
+                                </section>
+                            )
+                        }}
+                    </BookContext.Consumer>
+                )}
+            </ThemeContext.Consumer>
         )
     }
 }
